07128a414bddccc7f7ffd73e9e5bbe52f37e29fd
judiciousmurich <murichjudicious45@gmail.com>
2024-08-20 20:03:02 +0300

    feat: add values

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 function Message({ message }) {
   const isSent = message.sender === 'You';
   return (
-    <div
-      className={`mb-2 flex ${isSent ? 'justify-end' : 'justify-start'}`}
-    >
+    <div className={`mb-2 flex ${isSent ? 'justify-end' : 'justify-start'}`}>
       <div
         className={`p-3 rounded-lg max-w-xs ${
-          isSent ? 'bg-blue-500 text-white' : 'bg-gray-200'
+          isSent
+            ? 'bg-[#056162] text-[#E9EDEF]'  // Sent message color
+            : 'bg-[#202C33] text-[#E9EDEF]'  // Received message color
         }`}
       >
         <p>{message.text}</p>
