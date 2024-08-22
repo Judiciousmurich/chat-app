3cfb62ffb013bcb30b738b7712451bfc50d90c38
judiciousmurich <murichjudicious45@gmail.com>
2024-08-22 23:36:12 +0300

    feat: add values

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const [currentChat, setCurrentChat] = useState(chats[0]);
   const [messages, setMessages] = useState([
     { text: 'Hey', sender: 'Judy' },
-    { text: 'Hi, how are you?', sender: 'You' },
+    { text: 'Hi,, sender: 'You' },
   ]);
 
   const sendMessage = (message) => {
